04fb41b9927d39f9bd6dc4597aa7eae207a3f6d2
Naxum <jake@naxite.com>
2017-07-10 22:12:51 -0500

    Organize project, start work on sticky note wall collection view

---
+++ Stickies/StickyCell.swift
@@ -0,0 +1,29 @@
+//
+//  StickyCell.swift
+//  Stickies
+//
+//  Created by Jacob Sawyer on 7/10/17.
+//  Copyright © 2017 Jake Sawyer. All rights reserved.
+//
+
+import UIKit
+
+class StickyCell: UICollectionViewCell {
+
+    @IBOutlet fileprivate weak var thumbnailView: UIImageView! {
+        didSet {
+            thumbnailView.image = thumbnail
+        }
+    }
+    
+    var thumbnail:UIImage? {
+        didSet {
+            thumbnailView?.image = thumbnail
+        }
+    }
+    
+    override func prepareForReuse() {
+        thumbnail = nil
+    }
+
+}
