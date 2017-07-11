3d76b61d97acf5768473b54310fb13d5087bde7d
Naxum <jake@naxite.com>
2017-07-10 21:59:55 -0500

    Initial Commit

---
+++ Stickies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stickies
+//
+//  Created by Jacob Sawyer on 7/10/17.
+//  Copyright © 2017 Jake Sawyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
